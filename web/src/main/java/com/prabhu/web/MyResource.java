eb422082f5a4057a931acbf2ccc55c54d6348e2e
prabhu <prabhu@ozthra.com>
2017-02-20 23:38:14 -0500

    Changes

--- web/src/main/java/com/prabhu/web/MyResource.java
+++ web/src/main/java/com/prabhu/web/MyResource.java
@@ -1,9 +1,19 @@
 package com.prabhu.web;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.server.ResourceConfig;
+
+import com.prabhu.annotations.Audit;
+import com.prabhu.filter.AuditFilter;
+import com.prabhu.filter.AuditFilterNew;
+import com.prabhu.web.model.InputModel;
+import com.prabhu.web.model.OutputModel;
 
 @Path("/myresource")
 
@@ -14,8 +24,20 @@ public class MyResource extends ResourceConfig{
 //	}
 
 	@GET
+	@Path("name")
+	@Audit(clazz=AuditFilterNew.class)
 	public String name() {
 		return "hello World";
+		
+	}
+	
+	@POST
+	@Path("details")
+	@Audit(clazz=AuditFilter.class)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public OutputModel getDetails(InputModel inputModel) {
+		return new OutputModel("sai", "1234");
 		
 	}
 }
