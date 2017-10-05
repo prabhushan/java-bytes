c287346abe8b104a9d2e707ebe27fb68d628bc07
prabhu <prabhu@ozthra.com>
2017-10-04 22:35:24 -0400

    Working log4J

--- mock-service/src/main/java/com/drfirst/mock/MockServiceApplication.java
+++ mock-service/src/main/java/com/drfirst/mock/MockServiceApplication.java
@@ -1,13 +1,23 @@
 package com.drfirst.mock;
 
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MockServiceApplication {
+public class MockServiceApplication implements CommandLineRunner {
 
+    private static Logger log = Logger.getLogger(MockServiceApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MockServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("test");
+		
 	}
 }
