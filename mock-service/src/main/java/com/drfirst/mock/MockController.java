c3e9ee5513de8e139c1fa18ca1c2ef6f0646ca17
prabhu <prabhu@ozthra.com>
2017-10-06 08:34:31 -0400

    Changes to exclude log4j properties

--- mock-service/src/main/java/com/drfirst/mock/MockController.java
+++ mock-service/src/main/java/com/drfirst/mock/MockController.java
@@ -3,9 +3,9 @@ package com.drfirst.mock;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,20 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MockController {
 
-	static Logger logger = LoggerFactory.getLogger(MockController.class);
+	private static Logger logger = Logger.getLogger(MockServiceApplication.class);
 
-	Map<String,String> mockMap = new ConcurrentHashMap<>();
+	Map<String, String> mockMap = new ConcurrentHashMap<>();
+
 	@RequestMapping(path = { "/set/mock" }, method = RequestMethod.POST, consumes = {
 			"application/x-www-form-urlencoded" })
-	public String setMock(@RequestParam("mockPath") String mockPath,@RequestParam("mockResponse") String mockResponse) {
-		logger.info("test" + mockPath + mockResponse);
+	public String setMock(@RequestParam("mockPath") String mockPath,
+			@RequestParam("mockResponse") String mockResponse) {
+		logger.info("Mock Set" + mockPath + mockResponse);
 		mockMap.put(mockPath, mockResponse);
-		return "OK";
+		return "Mock set for url " + mockPath;
 	}
-	
-	@RequestMapping(path="/{pathParam}", method = {RequestMethod.POST,RequestMethod.GET})
-	public String getMock(@PathVariable("pathParam") String pathParam){
-		return mockMap.get(pathParam);
+
+	@RequestMapping(path = "/{pathParam}", method = { RequestMethod.POST, RequestMethod.GET })
+	public String getMock(@PathVariable("pathParam") String pathParam,
+			@RequestHeader(value = "Accept") String acceptType) {
+		String response = mockMap.get(pathParam);
+		logger.info(acceptType);
+		return response;
 	}
 
 }
